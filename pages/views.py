db27bd0f3f322b2805745afe309f64f7f095f74d
swapnil butia <swapnilbutia05@gmail.com>
2021-06-28 18:54:26 +0530

    first commit

---
+++ pages/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import Team
+
+
+# Create your views here.
+def home(request):
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+        }
+    return render(request,'pages/home.html', data)
+
+
+def about(request):
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+        }
+    return render(request,'pages/about.html', data)
+
+
+
+def services(request):
+    return render(request,'pages/services.html')
+
+
+
+
+def contact(request):
+    return render(request,'pages/contact.html')
